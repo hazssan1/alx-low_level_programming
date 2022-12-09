0e3d865a91721a6d64968225f2d442dfdaf6c542
hazssan1 <hazssan1@gmail.com>
2022-12-09 01:24:21 -0800

    quote c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -  print piece of art is useful 
- * code by mohammed hassan 
+ * main -print piece of art is useful
  * Return: 1
 */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(1, "and that piece of art is useful\" -Dora Korpar,2015-10-19\n", 59);
 	return (1);
+}
