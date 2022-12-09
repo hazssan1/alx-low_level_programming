a148da7d70ff5a652b5ade225e82e3cfd5987a8c
hazssan1 <hazssan1@gmail.com>
2022-12-09 08:11:22 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	long long int d;
 	float f;
 	print("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	print("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	print("Size of an int: %lu byte(s)\n", (unsigned long)<F11>sizeof(b));
 	print("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	print("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	print("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
