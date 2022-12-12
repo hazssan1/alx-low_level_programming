563ab7c704d19d76f0cb98921959c4cf44a8907e
hazssan1 <hazssan1@gmail.com>
2022-12-12 04:27:11 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: printing single digital number of base 10 starting from 01
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
