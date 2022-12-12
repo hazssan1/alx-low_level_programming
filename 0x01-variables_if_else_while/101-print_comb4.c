593d40c4ec6038bf7102442fbc3600569198469a
hazssan1 <hazssan1@gmail.com>
2022-12-12 08:14:31 -0800

    print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - a simple program that outputs 0-9 separated by commas
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i < 56; i++)
+	{
+		for (j = i + 1; j < 57; j++)
+		{
+			for (k = j + 1; k < 58; k++)
+			{
+				putchar(i);
+				pitchar(j);
+				pitchar(k);
+				if (i != 55 || j != 56 || k != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
