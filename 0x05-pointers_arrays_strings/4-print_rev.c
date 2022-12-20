3891fb9add48a474cbbf36c9631da42765fd5924
hazssan1 <hazssan1@gmail.com>
2022-12-20 12:12:37 -0800

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: strings  to be print
+ * muhammed hassan
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	j = 0;
+	while (s[j] != '\0')
+		j++;
+
+
+	for (i = j - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar(10);
+}
