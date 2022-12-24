5fc492011a4afeda417cf5176fcec33ba0e33564
hazssan1 <hazssan1@gmail.com>
2022-12-24 10:03:55 -0800

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -16,7 +16,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	for (i = 0; n1[i]; i++)
 
 	for (j = 0; n2[j]; j++)
-	
+
 	if (i > size_r || j > size_r)
 		return (0);
 	m = 0;
@@ -37,11 +37,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	r[k] = '\0';
 	if (i >= 0 || j >= 0 || m)
 		return (0);
-	 for (k -= 1, l = 0; l < k; k--, l++)
-	 {
-		 m = r[k];
-		 r[k] = r[l];
-		 r[l] = m;
-	 }
-	 return (r);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
